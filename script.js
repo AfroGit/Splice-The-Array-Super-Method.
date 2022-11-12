636487c3578d53b4503827ec033d3933747d8141
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2022-11-12 00:54:22 +0000

    Gave my program the ability to modify Objects.

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// arr.splice(start[, deleteCount, elem1, ..., elemN])
+
+let numFlow = [4545, 65657, 93931,53537, 131313, 337337];
+  
+ numFlow.splice(1, 2, 24246, 84842, 46468);
+  
+ console.log(numFlow)
